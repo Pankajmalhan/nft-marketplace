ae37337b0322c2267d3958a83c5f093a8691ee5c
PankajC-Tft <pankajc@applause.stream>
2022-10-28 18:49:15 +0530

    setup ether.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-
 /* eslint-disable @next/next/no-img-element */
 
-import type { NextPage } from 'next';
-import { BaseLayout, NftList } from '@ui';
+import type { NextPage } from "next";
+import { BaseLayout, NftList } from "@ui";
 import nfts from "../content/meta.json";
-import { NftMeta } from '@_types/nft';
+import { NftMeta } from "@_types/nft";
+import { useWeb3 } from "@ui/providers/web3";
 
 const Home: NextPage = () => {
+  const { provider } = useWeb3();
+  const getAccounts = async () => {
+    const accounts = await provider!.listAccounts();
+    console.log(accounts);
+  };
+
+  if (provider) {
+    getAccounts();
+  }
+
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -15,18 +25,18 @@ const Home: NextPage = () => {
         </div>
         <div className="relative">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures NFTs</h2>
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+              Amazing Creatures NFTs
+            </h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList
-            nfts={nfts as NftMeta[]}
-          />
+          <NftList nfts={nfts as NftMeta[]} />
         </div>
       </div>
     </BaseLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
